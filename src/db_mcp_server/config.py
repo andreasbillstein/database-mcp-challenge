cbab0a2f0f5a993151058c7e99d760f074aeb2b8
Andreas Billstein <andreas.billstein@gmx.de>
2026-06-09 21:42:28 +0200

    fix ci failure

--- src/db_mcp_server/config.py
+++ src/db_mcp_server/config.py
@@ -4,10 +4,13 @@ from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from db_mcp_server.backends.sqlalchemy import PostgresBackendConfig, SQLiteBackendConfig
+
 BackendConfig = Annotated[
     SQLiteBackendConfig | PostgresBackendConfig,
     Field(discriminator="kind"),
 ]
+
+
 class Config(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
